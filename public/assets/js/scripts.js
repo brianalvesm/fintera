c879437352fb6420982cc46502c88c7084c2f253
Brian Alves Moreira <hey@brianmoreira.com>
2021-12-10 17:46:38 -0300

    cria componente de nova seleção de perfil empresarial no mobile

--- public/assets/js/scripts.js
+++ public/assets/js/scripts.js
@@ -1,3 +1,21 @@
 $('.bg-preview-btn').click( function() {
   $('.bg-preview').toggleClass('bg-active');
 });
+
+
+var profileItems = function () {
+
+  if ($(window).width() < 640) {
+    $('.profile-item.active').toggleClass('closed');
+    $('.profile-item.active').nextAll().addClass('hide');
+
+    $('.profile-item.active').click( function() {
+      $(this).nextAll().toggleClass('hide');
+      $(this).toggleClass('closed');
+    })
+  }
+};
+
+$(document).ready(function () {
+  profileItems();
+});
