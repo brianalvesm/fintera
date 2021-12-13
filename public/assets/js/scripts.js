f5fde3001b698abc36d7ac0c28a4a0f7774f8620
Brian Alves Moreira <hey@brianmoreira.com>
2021-12-13 11:46:49 -0300

    Ajustes gerais

--- public/assets/js/scripts.js
+++ public/assets/js/scripts.js
@@ -2,24 +2,27 @@ $('.bg-preview-btn').click( function() {
   $('.bg-preview').toggleClass('bg-active');
 });
 
-
-var profileItems = function () {
-
-  if ($(window).width() < 640) {
-    $('.profile-item.active').toggleClass('closed');
-    $('.profile-item.active').nextAll().addClass('hide');
-
-    $('.profile-item.active').click( function() {
-      $(this).nextAll().toggleClass('hide');
-      $(this).toggleClass('closed');
-    })
-  }
-};
-
 $(document).ready(function () {
-  profileItems();
+
+  var profileItems = function () {
+
+    if ($(window).width() < 640) {
+      $('.profile-item.active').addClass('closed');
+      $('.profile-item.active').nextAll().addClass('hide');
+
+      $('.profile-item.active').click( function() {
+        $(this).toggleClass('closed');
+        $(this).nextAll().toggleClass('hide');
+      })
+    } else {
+      $('.profile-item.active').removeClass('closed');
+      $('.profile-item.active').nextAll().removeClass('hide');
+    }
+  };
 
   $(window).resize(function() {
     profileItems();    
   });
+
+  profileItems();
 });
